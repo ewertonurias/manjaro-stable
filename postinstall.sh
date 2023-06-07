1b67563904254e2e08683c39997ff253f1bd7100
ewertonurias <26447762+ewertonurias@users.noreply.github.com>
2023-06-06 23:00:16 -0400

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 
 ### Gera lista com os 5 espelhos mais rápidos
-printf "\nEtapa 1/5: Gerando lista de espelhos... \n\n"
+printf "\nEtapa 1/6: Gerando lista de espelhos... \n\n"
 sudo pacman-mirrors -f5
 
 ### Atualização completa dos pacotes
-printf "\nEtapa 2/5: Atualizando o sistema... \n\n"
+printf "\nEtapa 2/6: Atualizando o sistema... \n\n"
 sudo pacman -Syyu
 
 ### Habilita o TRIM no SSD
-printf "\nEtapa 3/5: Habilitando o TRIM para o SSD... \n\n"
+printf "\nEtapa 3/6: Habilitando o TRIM para o SSD... \n\n"
 sudo systemctl enable fstrim.timer
 
 ### Instala o driver proprietário NVIDIA automaticamente
-printf "\nEtapa 4/5: Instalando driver proprietário NVIDIA... \n\n"
+printf "\nEtapa 4/6: Instalando driver proprietário NVIDIA... \n\n"
 sudo mhwd -a pci nonfree 0300
 
 ### Instala minha seleção de pacotes
-printf "\nEtapa 5/5: Instalando minha seleção de softwares... \n\n"
-sudo pacman -Syu k{df,rita,colorchooser} inkscape libreoffice-still-pt-br qbittorrent unrar steam telegram-desktop youtube-dl
+printf "\nEtapa 5/6: Instalando minha seleção de softwares... \n\n"
+sudo pacman -Syu k{df,rita,colorchooser} inkscape qbittorrent unrar steam telegram-desktop youtube-dl
+
+### Instala o Discord do Flathub
+printf "\nEtapa 6/6: Instalando o Discord do Flathub... \n\n"
+flatpak install flathub com.discordapp.Discord
 
 ### Fim do script.
 printf "\nFIM DO SCRIPT \n\n"
