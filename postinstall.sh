0e5e1681f265cc970cf0174f9d81d312328e230e
ewertonurias <26447762+ewertonurias@users.noreply.github.com>
2023-09-30 10:41:24 -0400

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -3,24 +3,28 @@
 ### Gera lista com os 5 espelhos mais rápidos
 sudo pacman-mirrors -f5
 
-### Atualização completa de pacotes
+### Habilita download paralelo no Pacman
+sudo nano /etc/pacman.conf
+#Encontre e descomente a linha "ParallelDownloads = 5"
+
+### Atualiza banco de dados e todos os pacotes no sistema
 sudo pacman -Syu
 
-### Criar diretório e montar partição automaticamente
+### Cria diretório e monta partição automaticamente
 sudo mkdir /mnt/games
 sudo nano /etc/fstab
-#UUID=f4525d07-661d-4c65-92b5-4dfaddf54c51 /mnt/games     ext4    defaults                   0 0
+#No final do arquivo, adicione a linha: UUID=f4525d07-661d-4c65-92b5-4dfaddf54c51 /mnt/games     ext4    defaults                   0 0
 
 ### Habilita o TRIM no SSD
 sudo systemctl enable fstrim.timer
 
-### Instala minha seleção de pacotes
+### Instala minha seleção pessoal de pacotes
 sudo pacman -Syu feh inkscape k{df,rita,colorchooser,denlive} obs-studio qbittorrent steam telegram-desktop unrar yt-dlp
 
 ### Saída de áudio para perifêricos simultâneos
 sudo pacman -Syu paprefs
 
-### Wine-Staging
+### Instala o Wine-Staging
 sudo pacman -Syu wine-staging
 
 ### Instala o Discord do Flathub
