2a3850907fe480eaca2645f29507ef2b09488f73
ewertonurias <26447762+ewertonurias@users.noreply.github.com>
2023-09-29 23:31:06 -0400

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -1,28 +1,33 @@
 #!/bin/bash
 
 ### Gera lista com os 5 espelhos mais rápidos
-printf "\nEtapa 1/6: Gerando lista de espelhos... \n\n"
 sudo pacman-mirrors -f5
 
-### Atualização completa dos pacotes
-printf "\nEtapa 2/6: Atualizando o sistema... \n\n"
-sudo pacman -Syyu
+### Atualização completa de pacotes
+sudo pacman -Syu
+
+### Criar diretório e montar partição automaticamente
+sudo mkdir /mnt/games
+sudo nano /etc/fstab
+#UUID=f4525d07-661d-4c65-92b5-4dfaddf54c51 /mnt/games     ext4    defaults                   0 0
 
 ### Habilita o TRIM no SSD
-printf "\nEtapa 3/6: Habilitando o TRIM para o SSD... \n\n"
 sudo systemctl enable fstrim.timer
 
-### Instala o driver proprietário NVIDIA automaticamente
-printf "\nEtapa 4/6: Instalando driver proprietário NVIDIA... \n\n"
-sudo mhwd -a pci nonfree 0300
-
 ### Instala minha seleção de pacotes
-printf "\nEtapa 5/6: Instalando minha seleção de softwares... \n\n"
-sudo pacman -Syu k{df,rita,colorchooser} inkscape qbittorrent unrar steam telegram-desktop youtube-dl
+sudo pacman -Syu feh inkscape k{df,rita,colorchooser,denlive} obs-studio qbittorrent steam telegram-desktop unrar yt-dlp
+
+### Saída de áudio para perifêricos simultâneos
+sudo pacman -Syu paprefs
+
+### Wine-Staging
+sudo pacman -Syu wine-staging
 
 ### Instala o Discord do Flathub
-printf "\nEtapa 6/6: Instalando o Discord do Flathub... \n\n"
 flatpak install flathub com.discordapp.Discord
 
-### Fim do script.
-printf "\nFIM DO SCRIPT \n\n"
+### Instala o Protontricks do Flathub e concede acesso à outra unidade de disco
+flatpak install flathub com.github.Matoking.protontricks
+flatpak override --user --filesystem=/mnt/games/SteamLibrary com.github.Matoking.protontricks
+
+### Fim do script
