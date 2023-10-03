5f9cfd77ba9e39b6734d4b0d90826e18394eed27
ewertonurias <26447762+ewertonurias@users.noreply.github.com>
2023-10-02 21:46:19 -0400

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -10,37 +10,16 @@ sudo nano /etc/pacman.conf
 ### Atualiza banco de dados e todos os pacotes no sistema
 sudo pacman -Syu
 
-### Cria diretório e monta partição automaticamente
-sudo mkdir /mnt/games
-sudo nano /etc/fstab
-#No final do arquivo, adicione a linha: UUID=f4525d07-661d-4c65-92b5-4dfaddf54c51 /mnt/games     ext4    defaults                   0 0
-
 ### Habilita o TRIM no SSD
 sudo systemctl enable fstrim.timer
 
 ### Instala minha seleção pessoal de pacotes
-sudo pacman -Syu feh inkscape k{df,rita,colorchooser,denlive} obs-studio qbittorrent steam telegram-desktop unrar yt-dlp
+sudo pacman -Syu feh inkscape k{df,rita,colorchooser,denlive} obs-studio qbittorrent telegram-desktop unrar yt-dlp
 
 ### Saída de áudio para perifêricos simultâneos
 sudo pacman -Syu paprefs
 
-### Instala o Wine-Staging
-sudo pacman -Syu wine-staging
-
-### Instala o Discord do Flathub
-flatpak install flathub com.discordapp.Discord
-
-### Instala o Protontricks do Flathub e concede acesso à outra unidade de disco
-flatpak install flathub com.github.Matoking.protontricks
-flatpak override --user --filesystem=/mnt/games/SteamLibrary com.github.Matoking.protontricks
-
 ### Instala a sessão Wayland
 sudo pacman -Syu plasma-wayland-session
-
-### Instala o driver Vulkan de código aberto RADV
-sudo pacman -Syu amdvlk lib32-amdvlk
-
-### Instala o Gamemode e MangoHud
-sudo pacman -Syu gamemode lib32-gamemode mangohud
 
 ### Fim do script
